0d7ced0b21afbdb79fcf632ebf29f933024ecf82
Kazuhiro HISHINUMA <kaz@cs.meiji.ac.jp>
2018-04-13 11:35:12 +0900

    Write setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name='ybook',
+      version='0.1.0',
+      author='Kazuhiro HISHINUMA',
+      author_email='kaz@cs.meiji.ac.jp',
+      url='https://github.com/kazh98/ybook',
+      description='Miscellaneous functions used for Meiji Univ. Seminar I',
+      long_description='This is a python package providing miscellaneous functions used for Seminar I, '
+                       'a Meiji University compulsory subject.',
+      download_url='https://github.com/kazh98/ybook/releases',
+      classifiers=['Topic :: Education',
+                   'Topic :: Scientific/Engineering',
+                   'Intended Audience :: Education',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: MIT License'],
+      py_modules=['ybook'],
+      )
